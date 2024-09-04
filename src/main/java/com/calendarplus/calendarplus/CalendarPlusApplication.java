f8370de0389ddac1397669bbd2036c5c9d253a92
ejum01 <ejum0713@gmail.com>
2024-09-04 18:43:17 +0900

    first commit

---
+++ src/main/java/com/calendarplus/calendarplus/CalendarPlusApplication.java
@@ -0,0 +1,13 @@
+package com.calendarplus.calendarplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calendarplus/calendarplus/CalendarPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendarplus.calendarplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
